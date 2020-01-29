3c20d189c44a4bf8d0623d5cd87f9a48b8ff07fc
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-29 22:05:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,12 @@ class Song
     
   end
   
-  def self.artist_count 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] += 1 
+        
+      
   end 
   
 end 
