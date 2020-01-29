94115978cefc4ff28839d3cc079e6ab7d01b5a5e
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-29 21:59:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,15 @@ class Song
   
   def self.genre_count
     genre_count = {}
-        
+    @@genres.each do |genre|
+      if genre_count[genre] #if the genre is already in the hash
+        genre_count[genre] += 1
+      else #if it's a new genre
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end    
     
   end
   
